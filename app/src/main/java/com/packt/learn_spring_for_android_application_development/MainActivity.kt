57ee2aaecfefb1b5514ddbd98cb85a5d688fdb18
Ihor Kucherenko <kuchernkoigor@gmail.com>
2018-09-13 16:14:43 +0300

    Add examples for the second chapter

--- app/src/main/java/com/packt/learn_spring_for_android_application_development/MainActivity.kt
+++ app/src/main/java/com/packt/learn_spring_for_android_application_development/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.packt.learn_spring_for_android_application_development
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,3 +10,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
---
+++ app/src/main/java/com/packt/learn_spring_for_android_application_development/chapter2/BasicSyntax.kt
@@ -0,0 +1,41 @@
+package com.packt.learn_spring_for_android_application_development.chapter2
+
+/**
+ * Created by ihor_kucherenko on 9/12/18.
+ * https://github.com/KucherenkoIhor
+ */
+
+
+val readOnly = 3
+var mutable = 3
+
+fun changeMutable() {
+    mutable = 4
+}
+
+class Foo {
+    val readOnly = 3
+    var mutable = 3
+
+    fun changeMutable() {
+        mutable = 4
+    }
+}
+
+class Bar {
+    companion object {
+        const val NAME = "Igor"
+
+        fun printName() = println(NAME)
+    }
+}
+
+fun test() {
+    Bar.NAME
+    Bar.printName()
+}
+
+
+//fun collection() {
+//    List()
+//}
