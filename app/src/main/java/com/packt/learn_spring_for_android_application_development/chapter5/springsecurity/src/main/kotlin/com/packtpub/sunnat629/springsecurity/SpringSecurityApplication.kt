7c990bad7c124d635366675446e3c684125ec51c
S M Mohi Us Sunnat <suncha629@gmail.com>
2018-11-14 12:14:14 +0600

    deleted the .gitignore from the root folder and added the chapter 5 folder and a demo project

---
+++ app/src/main/java/com/packt/learn_spring_for_android_application_development/chapter5/springsecurity/src/main/kotlin/com/packtpub/sunnat629/springsecurity/SpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package com.packtpub.sunnat629.springsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringSecurityApplication>(*args)
+}
