b1846ea3f0ff2dfeb7f5631bbbd0a0a4b075ad40
S M Mohi Us Sunnat <suncha629@gmail.com>
2018-11-28 21:38:40 +0600

    added the sql file

---
+++ app/src/main/java/com/packt/learn_spring_for_android_application_development/chapter6/jpa_db_test/src/main/resources/query.sql
@@ -0,0 +1,5 @@
+insert into cha6_dbtest_schema.users (name, email, contact_number) VALUE ('Hasib', 'sunnat629@gmail.com', '1234567890');
+insert into cha6_dbtest_schema.users (name, email, contact_number) VALUE ('Chaity', 'chaity629@gmail.com', '234324');
+insert into cha6_dbtest_schema.users (name, email, contact_number) VALUE ('Mirza', 'mirza629@gmail.com', '6576657');
+insert into cha6_dbtest_schema.users (name, email, contact_number) VALUE ('Hasib', 'hasib629@gmail.com', '123123123');
+insert into cha6_dbtest_schema.users (name, email, contact_number) VALUE ('Jisan', 'jisan629@gmail.com', '678768');
