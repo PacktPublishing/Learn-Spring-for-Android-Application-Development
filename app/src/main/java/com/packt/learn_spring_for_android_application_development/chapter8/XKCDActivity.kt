ae47a6d6e0ae70076e5af1297a75c74a6892547c
Ihor Kucherenko <kuchernkoigor@gmail.com>
2019-01-10 20:03:02 +0200

    Fixes XKCD

--- app/src/main/java/com/packt/learn_spring_for_android_application_development/chapter8/XKCDActivity.kt
+++ app/src/main/java/com/packt/learn_spring_for_android_application_development/chapter8/XKCDActivity.kt
@@ -1,18 +1,15 @@
 package com.packt.learn_spring_for_android_application_development.chapter8
 
 import android.graphics.Bitmap
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.support.annotation.UiThread
+import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
-import android.widget.Toast
 import com.packt.learn_spring_for_android_application_development.R
 import com.packt.learn_spring_for_android_application_development.chapter7.Comic
 import com.packt.learn_spring_for_android_application_development.chapter7.loadBitmap
 import com.packt.learn_spring_for_android_application_development.chapter7.loadComic
-import org.reactivestreams.Subscriber
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
 
